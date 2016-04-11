ca81f0203ee747d1a3e6eb2b1f0ff7b2e534ba01
黄金龙 <huangjinlong@neunn.com>
2016-04-11 11:42:45 +0800

    new

---
+++ Mammon/src/main/java/com/lokihjl/mammon/producer/LogProducer.java
@@ -0,0 +1,37 @@
+package com.lokihjl.mammon.producer;
+
+import java.util.Properties;
+
+import com.lokihjl.mammon.utils.StringUtils;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+/**
+ * @author hjl
+ * @E-mail:huangjl@neunn.com
+ * @version 创建时间：2016年4月8日 下午5:16:43
+ */
+public class LogProducer {
+    
+    private Producer<String, String> inner ;
+    
+    public LogProducer() throws Exception{
+        Properties properties = new Properties() ;
+        
+        properties.load(ClassLoader.getSystemResourceAsStream("producer.properties")); 
+        ProducerConfig config = new ProducerConfig(properties) ;
+        inner = new Producer<String, String>(config) ;
+    }
+    
+    public void send(String topicName, String message) {
+        if(StringUtils.isEmpty(topicName) || StringUtils.isEmpty(message)) {
+            return ;
+        }
+        
+        KeyedMessage<String, String> km = new KeyedMessage<String, String>(topicName, message) ;
+        inner.send(km);
+    }
+
+}
---
+++ Mammon/src/main/java/com/lokihjl/mammon/utils/StringUtils.java
@@ -0,0 +1,25 @@
+package com.lokihjl.mammon.utils;
+/**
+ * @author hjl
+ * @E-mail:huangjl@neunn.com
+ * @version 创建时间：2016年4月8日 下午5:29:42
+ */
+public class StringUtils {
+    
+    public static boolean isEmpty(final String str) {
+        if(null == str || str.trim().equals("")) {
+            return true ;
+        }
+        
+        return false ;
+    }
+    
+    public static boolean isNoEmpty(final String str) {
+        if(isEmpty(str)) {
+            return false ;
+        }
+        
+        return true ;
+    }
+
+}
